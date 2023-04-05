52147d70a194a08210dedc1f2d9c706d56f2fd3f
vl <vladimir.luchaninov@gmail.com>
2023-04-05 13:50:37 +0200

    ArrayRandom

---
+++ src/ArrayUtils/ArrayRandom.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Gupalo\ArrayUtils;
+
+class ArrayRandom
+{
+    public static function pick(array $items): mixed
+    {
+        if (!$items) {
+            return null;
+        }
+
+        return $items[array_rand($items)];
+    }
+
+    public static function pickMultiple(array $items, int $count = 1, bool $preserveKeys = false, mixed $default = []): mixed
+    {
+        if (!$items || $count < 1) {
+            return $default;
+        }
+
+        $keys = array_rand($items, $count);
+        if ($count === 1) {
+            $keys = [$keys]; // array_rand returns "key" when count=1
+        }
+
+        $result = [];
+        foreach ($keys as $key) {
+            if ($preserveKeys) {
+                $result[$key] = $items[$key];
+            } else {
+                $result[] = $items[$key];
+            }
+        }
+
+        return $result;
+    }
+}
---
+++ tests/ArrayUtils/ArrayRandomTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Gupalo\Tests\ArrayUtils;
+
+use Gupalo\ArrayUtils\ArrayRandom;
+use PHPUnit\Framework\TestCase;
+
+class ArrayRandomTest extends TestCase
+{
+    public function testPick(): void
+    {
+        self::assertNull(ArrayRandom::pick([]));
+        self::assertTrue(ArrayRandom::pick([true]));
+        self::assertSame(1, ArrayRandom::pick([1]));
+        self::assertSame('1', ArrayRandom::pick(['1']));
+        self::assertSame('1', ArrayRandom::pick(['1', '1', '1']));
+        self::assertContains(ArrayRandom::pick(['a', 'b', 'c']), ['a', 'b', 'c']);
+    }
+
+    public function testPickMultiple(): void
+    {
+        self::assertSame([], ArrayRandom::pickMultiple([]));
+        self::assertNull(ArrayRandom::pickMultiple([], default: null));
+        self::assertSame([true], ArrayRandom::pickMultiple([true]));
+        self::assertSame([1], ArrayRandom::pickMultiple([1]));
+        self::assertSame(['1'], ArrayRandom::pickMultiple(['1']));
+        self::assertSame(['1'], ArrayRandom::pickMultiple(['1', '1', '1'], preserveKeys: false));
+        self::assertContains(ArrayRandom::pickMultiple(['a', 'b', 'c']), [['a'], ['b'], ['c']]);
+
+        self::assertContains(ArrayRandom::pickMultiple(['a', 'b'], count: 2), [['a', 'b'], ['b', 'a']]);
+        self::assertContains(ArrayRandom::pickMultiple(['k1' => 'a', 'k2' => 'b'], count: 2), [['a', 'b'], ['b', 'a']]);
+        self::assertContains(ArrayRandom::pickMultiple(['k1' => 'a', 'k2' => 'b'], count: 2, preserveKeys: true), [['k1' => 'a', 'k2' => 'b'], ['k2' => 'b', 'k1' => 'a']]);
+    }
+}
